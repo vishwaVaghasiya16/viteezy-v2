52ec7a8b639453e116235defc9a1160fd3791d3a
vishwaVaghasiya16 <vishwa.logicgoinfotech@gmail.com>
2025-11-26 11:51:16 +0530

    Payment process

--- v-1/routes/products.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getProducts } from "../controllers/products.controller.js";
-
-const router = Router();
-
-router.get("/products", getProducts);
-
-export default router;
